e91f14818fcfb8961e74009a7c5f9a6ec2d87e29
José Nahuel Cuesta Luengo <nahuelcuestaluengo@gmail.com>
2012-12-13 08:53:07 -0300

    Refactored things a little bit to improve the usability of the JS helper.

--- Makefile
+++ Makefile
@@ -11,13 +11,13 @@ css:
 css-min:
 		sass --update --style compressed $(SRC_FILE):$(OUTPUT)/indicator.min.css
 
-js-min:
+js-hint:
 		jshint --config=js/.jshintrc js/*.js
 
 all:
 		make clean
 		make css
 		make css-min
-		make js-min
+		make js-hint
 
-.PHONY: all clean css css-min js-min
+.PHONY: all clean css css-min js-hint
--- js/css-indicator-generator.js
+++ js/css-indicator-generator.js
@@ -1,16 +1,20 @@
 (function ($) {
-    var types = {
+    // Themes definition hash
+    var themes = {
+        // Spinner theme
         'spinner': {
             content: '<span class="bar"></span>',
             usesMultiplier: true,
             multiplierOption: 'bars'
         },
+        // Bouncer theme
         'bouncer': {
             content: '<span class="ball"></span>',
             usesMultiplier: false
         }
-        
     };
+
+    // Indicators constructor.
     var Indicator = function (element, options) {
         this.options = $.extend({}, $.fn.indicator.defaults, options);
         this.$target = $(element);
@@ -18,46 +22,62 @@
         this.convert();
     };
 
+    // Add the required attributes and content to the current element
+    // in order to turn it into a css-indicator as defined by the
+    // already set options.
     Indicator.prototype.convert = function () {
-        var classes = ['indicator', this.options.type, this.options.size, this.options.extra].join(' '),
+        var classes = ['indicator', this.options.theme, this.options.size, this.options.extra].join(' '),
             content = this.getContent();
 
         this.$target.addClass(classes).html(content);
     };
 
+    // Get the HTML content for the theme according to the options
+    // set to the instance of the Indicator.
+    //
+    // @return string
     Indicator.prototype.getContent = function () {
-        var typeInfo = types[this.options.type],
-            content = typeInfo.content;
+        var themeInfo = themes[this.options.theme],
+            content = themeInfo.content,
+            times;
 
-        if (typeInfo.usesMultiplier) {
-            var times = this.options[typeInfo.multiplierOption] + 1;
-
+        if (themeInfo.usesMultiplier) {
+            times = this.options[themeInfo.multiplierOption] + 1;
             content = new Array(times).join(content);
         }
 
         return content;
     };
-    
-    // jQuery plugin definition
-    $.fn.indicator = function (opts) {
-        return this.each(function () {
-            var $this = $(this),
-                data = $this.data('indicator'),
-                options = typeof opts == 'object' && opts;
 
-            if (!data) {
-                $this.data('indicator', new Indicator(this, options));
-            }
+    // jQuery plugin definition.
+    //
+    // Usage:
+    // <code>
+    //   // Using defaults
+    //   $('#my-indicator').indicator();
+    //   // Just indicating the theme
+    //   $('#my-indicator').indicator('bouncer');
+    //   // Indicating a hash of options
+    //   $('#my-indicator').indicator({ theme: 'bouncer', size: 'small' });
+    // </code>
+    $.fn.indicator = function (opts) {
+        var options = {};
+
+        if (typeof opts == 'string') {
+            options.theme = opts;
+        } else {
+            options = typeof opts == 'object' && opts;
+        }
+
+        return this.each(function () {
+            new Indicator(this, options);
         });
     };
-
     $.fn.indicator.Constructor = Indicator;
-
     $.fn.indicator.defaults = {
-        type:  'spinner',   // The theme to use: 'spinner' or 'bouncer'.
+        theme: 'spinner',   // The theme to use: 'spinner' or 'bouncer'.
         size:  'normal',    // The size: 'small', 'medium', 'normal', 'large', 'x-large'.
         extra: '',          // Extra classes to define on the container.
-        bars:  9            // Only valid for spinner theme.
+        bars:  9            // Only valid for the 'spinner' theme.
     };
-
 })(window.jQuery);
